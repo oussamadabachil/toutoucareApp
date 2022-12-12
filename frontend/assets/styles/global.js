097d822c64afa2ba177fec3e774d2c791b6da8b4
OussamaDabachil <oussamadabachilpro@gmail.com>
2022-12-12 14:53:18 +0100

    creating globl js for style

---
+++ frontend/assets/styles/global.js
@@ -0,0 +1,37 @@
+/*
+
+Button 1 :
+
+ <TouchableOpacity style={styles.button}>
+        <Text style={styles.textButton}>Réserver</Text>
+ </TouchableOpacity>
+
+
+
+
+
+ button: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 1,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
+    backgroundColor: "#008486",
+    borderRadius: 13,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+
+
+textButton: {
+    color: "#fff",
+    fontWeight: "bold",
+
+    fontSize: 20,
+  },
+
+
+*/
