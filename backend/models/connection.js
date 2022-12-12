833d67c77db76a55a684c82ed2c50eef05d50720
Coralietur <115960080+Coralietur@users.noreply.github.com>
2022-12-12 17:47:57 +0100

    backend fonctionnel

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://Coralietur:cGgI9NNZcCFpsQTd@cluster0.jegajox.mongodb.net/ToutouCare";
+const connectionString = process.env.CONNECTION_STRING;
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))
