0d2cca4f3e89988761c6f89b2aefc819b27af2f6
Coralietur <115960080+Coralietur@users.noreply.github.com>
2022-12-19 16:58:25 +0100

    post présentation

--- frontend/screens/UserProfile.js
+++ frontend/screens/UserProfile.js
@@ -26,7 +26,7 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons/faPenToSquare';
 
 import UploadImage from './UploadImage';
 
-const BACKEND_ADDRESS = 'http://192.168.10.170';
+const BACKEND_ADDRESS = 'http://192.168.10.180';
 
 export default function UserProfile() {
 
