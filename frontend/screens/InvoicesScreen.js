e6e0a4a4bc08ada51ece64abcb7e49d5f891a9e1
OussamaDabachil <oussamadabachilpro@gmail.com>
2022-12-22 12:26:56 +0100

    oussama

--- frontend/screens/InvoicesScreen.js
+++ frontend/screens/InvoicesScreen.js
@@ -135,6 +135,7 @@ const styles = StyleSheet.create({
   },
   input: {
     //width: "80%",
+    //height: 40,
     //marginTop: 25,
     //borderBottomColor: "#ec6e5b",
     //borderBottomWidth: 1,
